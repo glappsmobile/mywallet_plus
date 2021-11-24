751d97e82f1032d4f69624e34dc1bf1bdfac945a
Glauco Villas Boas <glappsmobile@gmail.com>
2021-11-24 20:38:24 -0300

    removed try catch from financialEventsController

--- src/controllers/financialEventsController.js
+++ src/controllers/financialEventsController.js
@@ -1,33 +1,28 @@
 import * as financialEventsService from '../services/financialEventsService.js';
 
 const createFinancialEvent = async (req, res) => {
-  try {
-    const { value, type } = req.body;
-    const { user } = res.locals;
+  const { value, type } = req.body;
+  const { user } = res.locals;
 
-    if (!value || !type) {
-      return res.sendStatus(400);
-    }
-
-    if (!['INCOME', 'OUTCOME'].includes(type)) {
-      return res.sendStatus(400);
-    }
-
-    if (value < 0) {
-      return res.sendStatus(400);
-    }
-
-    const financialEvent = await financialEventsService.createFinancialEvent({ userId: user.id, value, type });
-
-    if (financialEvent.length === 0) {
-      return res.sendStatus(500);
-    }
-
-    res.sendStatus(201);
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
+  if (!value || !type) {
+    return res.sendStatus(400);
   }
+
+  if (!['INCOME', 'OUTCOME'].includes(type)) {
+    return res.sendStatus(400);
+  }
+
+  if (value < 0) {
+    return res.sendStatus(400);
+  }
+
+  const financialEvent = await financialEventsService.createFinancialEvent({ userId: user.id, value, type });
+
+  if (financialEvent.length === 0) {
+    return res.sendStatus(500);
+  }
+
+  res.sendStatus(201);
 };
 
 const getFinancialEvents = async (req, res) => {
